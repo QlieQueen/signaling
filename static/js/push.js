8a10db3dd6d621301e0e5aad41de37acc94b5df0
OlieQueen <qq2841015@163.com>
2025-06-30 14:38:47 +0800

    Add: XRTC1.0-pc设置offer

--- static/js/push.js
+++ static/js/push.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var localVideo = document.getElementById("localVideo");
 var pushBtn = document.getElementById("pushBtn");
 
 pushBtn.addEventListener("click", startPush);
@@ -8,6 +9,10 @@ var uid = $("#uid").val();
 var streamName = $("#streamName").val();
 var audio = $("#audio").val();
 var video = $("#video").val();
+var offer = "";
+var pc;
+const config = {};
+var localStream;
 
 function startPush() {
     console.log("send push: /signaling/push");
@@ -16,13 +21,93 @@ function startPush() {
         {"uid": uid, "streamName": streamName, "audio": audio, "video": video},
         function(data, textStatus) {
             console.log("push response: " + JSON.stringify(data));
-            console.log("textStatus: ", textStatus);
             if ("success" == textStatus && 0 == data.errNo) {
                 $("#tips1").html("<font color='blue'>推流请求成功!</font>");
+                console.log("offer sdp: \n" + data.data.sdp);
+                offer = data.data;
+                pushStream();
             } else {
                 $("#tips1").html("<font color='red'>推流请求失败!</font>");
             }
         },
         "json"
     );
+}
+
+function pushStream() {
+    pc = new RTCPeerConnection(config);
+
+    pc.setRemoteDescription(offer).then(
+        setRemoteDescriptionSuccess,
+        setRemoteDescriptionError
+    );
+}
+
+window.addEventListener("message", function(event) {
+    if (event.origin != window.location.origin) {
+        return;
+    }
+
+    if (event.data.type) {
+        if (event.data.type == "SS_DIALOG_SUCCESS") {
+            console.log("用户同意屏幕共享, streamId: " + event.data.streamId);
+            startScreenStreamFrom(event.data.streamId);
+        } else if (event.data.type == "SS_DIALOG_CANCEL") {
+            console.log("用户取消屏幕共享");
+        }
+    }
+});
+
+function startScreenStreamFrom(streamId) {
+    var constraints = {
+        audio: false,
+        video: {
+            mandatory: {
+                chromeMediaSource: "desktop",
+                chromeMediaSourceId: streamId,
+                maxWidth: window.screen.width,
+                maxHeight: window.screen.height
+            }
+        }
+    }
+
+    navigator.mediaDevices.getUserMedia(constraints).then(
+        handleSuccess).catch(handleError);
+}
+
+function handleSuccess(stream) {
+    navigator.mediaDevices.getUserMedia({audio: true}).then(
+        function(audioStream) {
+            stream.addTrack(audioStream.getAudioTracks()[0]);
+            localVideo.srcObject = stream;
+            localStream = stream;
+            pc.addStream(stream);
+            pc.createAnswer().then(
+                CreateSessionDescriptionSuccess,
+                CreateSessionDescriptionError
+            );
+        }
+    ).catch(handleError);
+}
+
+function handleError(error) {
+    console.log("get user media error: " + error);
+}
+
+function setRemoteDescriptionSuccess() {
+    console.log("pc set remote description success");
+    console.log("request screen share");
+    window.postMessage({type: "SS_UI_REQUEST", text: "push"}, "*");
+}
+
+function CreateSessionDescriptionSuccess(answer) {
+    console.log("answer sdp: \n" + answer.sdp);
+}
+
+function setRemoteDescriptionError() {
+    console.log("pc set remote descroption error: " + error);
+}
+
+function CreateSessionDescriptionError() {
+    console.log("pc create answer error: " + error);
 }
